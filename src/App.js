9db5afb8127e95b358a2ff1e6c9a8a499d215efb
AlexToudic <alexandre.toudic@gmail.com>
2018-11-03 21:40:01 +0100

    :art: Add display names for profiling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Fragment } from 'react'
 
 import Experiment from './components/Experiment'
 
-export default () => (
+const App = () => (
   <Fragment>
     <Experiment />
     <div className='infos'>
       <p>
-        The only purpose of this demo is to experiment manipulating WebGL through JSX.
+        The only purpose of this demo is to experiment manipulating WebGL
+        through JSX.
         <br />
-        Check out sources on
-        {' '}
+        Check out sources on{' '}
         <a href='https://github.com/alextoudic/react-threejs'>GitHub</a>.
       </p>
       <a
@@ -24,3 +24,7 @@ export default () => (
     </div>
   </Fragment>
 )
+
+App.displayName = 'App'
+
+export default App
